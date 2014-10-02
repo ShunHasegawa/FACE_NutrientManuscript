deac72be4af00cb712739732bd4a6866e32c54bc
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-10-02 16:26:57 +0100

    it's working so make others as well

--- R/Analysis.R
+++ R/Analysis.R
@@ -6,12 +6,18 @@ source("R/Pckg.R")
 ## Extractable
 load("Data//FACE_Extractable_CO2Mean.RData")
 ext <- TrtMean
-ext$type <- factor(ifelse(ext$variable == "po", "Bray-extractable", "KCl-extractable"))
+ext <- within(ext, {
+  type <- "extractable"
+  metric <- "extractable"
+})
 
 ## IEM
 load("Data//FACE_IEM_CO2Mean.RData")
 iem <- TrtMean
-iem$type <- "IEM-adsorbed"
+iem <- within(iem, {
+  type <- "IEM"
+  metric <- "IEM"
+})
 
 ## Lysimter
 load("Data//FACE_Lysimeter_CO2Mean.RData")
@@ -21,27 +27,23 @@ lys <- TrtMean
 lys <- subsetD(lys, !variable %in% c("tc", "ic", "tn"))
 
 # po -> p
-# shallow -> Shallow, deep -> Deep
 # type: soil solution and depth
 lys <- within(lys, {
   variable <- factor(variable, labels = c("no", "nh", "p", "toc"))
-  depth <- factor(depth, labels = c("Shallow", "Deep"))
-  type <- paste("Soil solution", depth, sep = "-")
+  type <- paste("lys", depth, sep = "-")
   depth <- NULL
+  metric <- "Lysmtr"
 })
 
 ## Mineralisation
 load("Data//FACE_Mineralisation_CO2Mean.RData")
 mine <- TrtMean
 mine <- within(mine, {
-  type <- factor(variable, levels = c("nitrification", "n.min",
-                                      "ammonification", "p.min"),
-                 labels = c("Nitrification", "N mineralisation", 
-                             "Ammonification", "P mineralisation"))
-  
+  type <- variable
   variable <- recode(variable, "c('nitrification', 'n.min') = 'no'; 
                                     'p.min' = 'p';
                                     'ammonification' = 'nh'")
+  metric <- "Mineralisation"
 })
 
 ## merge the above data frame
@@ -51,50 +53,32 @@ ntrDF <- rbind.fill(list(ext, iem, lys, mine))
 # test #
 ########
 df <- subset(ntrDF, variable == "no")
-df$type <- factor(df$type, levels = c("KCl-extractable", "IEM-adsorbed",
-                                      "Soil solution-Shallow", "Soil solution-Deep", 
-                                      "Nitrification", "N mineralisation"))
+df$type <- factor(df$type)
+levels(df$type)
 
-science_theme <- theme(panel.grid.major = element_blank(),
-                       panel.grid.minor = element_blank(),
-                       axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
-                       legend.position = c(.9, .9),
-                       legend.title = element_blank())
+df$type <- factor(df$type, levels = c("extractable", "IEM",
+                                      "lys-shallow", "lys-deep", 
+                                      "nitrification", "n.min"))
 
 
+## Blank data frame ## 
 
-## Blank data frame ## I woudl like to use the same y range for soil solution
-#and for nitrificaiton and N mineralisation. Create dataframe to define y range
-#for each plot. This data frame actually doesn't plot anything but enable one to
-#define y range (or x range if one likes)
+# I woudl like to use the same y range for two layers of lysimeter and also for
+# nitrificaiton and N mineralisation. Create dataframe to define y range for
+# each plot. This data frame actually doesn't plot anything but enable one to
+# define y range (or x range if one likes)
 
-df$metric <- factor(ifelse(df$type %in% c("Soil solution-Shallow", "Soil solution-Deep"),
-                           "SoilSol", 
-                           ifelse(df$type %in% c("Nitrification", "N mineralisation"), 
-                                  "mine", as.character(df$type))))
-
-# range of soil solution
-SSrng <- with(subsetD(df, type %in% c("Soil solution-Shallow", "Soil solution-Deep")), 
-             c(min(Mean - SE, na.rm = TRUE), max(Mean + SE, na.rm = TRUE)))
-
-unique(df$type)
-
-
-ddply(subsetD(df, .(type),
-      .(type), summarise, yval = median(Mean))
-
-
-blankDF <- data.frame(date = df$date[1], 
-                      ,
-                      
-                        
-                        
-                        
-                        c(rep(SSrng[1], 3), 0,
-                               rep(SSrng[2], 3), 0),
-                      co2 = "amb")
-
-
+# compute min and max for each metric, then combine with type using expand.grid
+blankDF <- data.frame(
+  ddply(df, .(metric), function(x){
+    yval <- with(x, c(min(Mean - SE, na.rm = TRUE), max(Mean + SE, na.rm = TRUE)))
+    exDF <- expand.grid(yval = yval, type = unique(x$type))
+    return(exDF)
+  }
+  ), 
+  co2 = "amb",
+  date = df$date[1]
+  )
 
 
 pl <- plf()
@@ -103,6 +87,11 @@ ggsavePP(filename = "Output//Fig/test", plot = pl, width = 7, height = 7)
 
 theme_set(theme_bw())
 
+science_theme <- theme(panel.grid.major = element_blank(),
+                       panel.grid.minor = element_blank(),
+                       axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
+                       legend.position = c(.9, .9),
+                       legend.title = element_blank())
 
 plf <- function()
 {p <- ggplot(df, aes(x = date, y = Mean, group = co2))
@@ -124,27 +113,19 @@ p2 <- p + geom_line(aes(linetype = co2)) +
   scale_linetype_manual(values = c("solid", "dashed"), 
                         labels = c("Ambient", expression(eCO[2]))) +
   facet_wrap(~type, scales = "free_y", ncol = 2) +
+  geom_blank(aes(x = date, y = yval), data = blankDF) +
   science_theme
 
 # modify labels
-ylabs <- c(expression(KCl*-extractable~(mg~kg^"-1")),
-           expression(IEM*-adsorbed~(ng~cm^"-1"~d^"-1")),
-           expression(Soil~solution~(Shallow)~(mg~l^"-1")),
-           expression(Soil~solution~(Deep)~(mg~l^"-1")),
-           expression(Net~nitrification~(mg~kg^"-1"~d^"-1")),
-           expression(Net~N~mineralisation~(mg~kg^"-1"~d^"-1")))
+ylabs <- c(expression(A.~~KCl*-extractable~(mg~kg^"-1")),
+           expression(B.~~IEM*-adsorbed~(ng~cm^"-1"~d^"-1")),
+           expression(C.~~Soil~solution~(Shallow)~(mg~l^"-1")),
+           expression(D.~~Soil~solution~(Deep)~(mg~l^"-1")),
+           expression(E.~~Net~nitrification~(mg~kg^"-1"~d^"-1")),
+           expression(F.~~Net~N~mineralisation~(mg~kg^"-1"~d^"-1")))
 
 pl <- facet_wrap_labeller(p2, labels = ylabs)
 return(pl)
 }
 
 ggsavePP(filename = "Output//Fig/test", plot = pl, width = 7, height = 7)
-
-  
-  
-  geom_text(aes(x = xv, y = yv * .95, label = labels),
-            fontface = "bold",
-            hjust = 1,
-            data = subLabDF) +
-  facet_grid(variable~., scales= "free_y", labeller= facetLab) +
-  figTheme
