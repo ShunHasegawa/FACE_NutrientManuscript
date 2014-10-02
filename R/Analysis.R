231094c140596b320ab072beee14effec5c36dbd
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-10-02 15:15:40 +0100

    creating test figs

--- R/Analysis.R
+++ R/Analysis.R
@@ -47,5 +47,61 @@ mine <- within(mine, {
 ## merge the above data frame
 ntrDF <- rbind.fill(list(ext, iem, lys, mine))
 
+#######
+# test #
+########
+df <- subset(ntrDF, variable == "no")
+df$type <- factor(df$type, levels = c("KCl-extractable", "Soil solution-Shallow",
+                                      "Nitrification", "IEM-adsorbed", 
+                                      "Soil solution-Deep", "N mineralisation"))
+
+science_theme <- theme(panel.grid.major = element_blank(),
+                       panel.grid.minor = element_blank(),
+                       axis.text.x  = element_text(angle=45, vjust= 1, hjust = 1),
+                       legend.position = "top",
+                       legend.title = element_blank())
 
 
+theme_set(theme_bw())
+
+
+p <- ggplot(df, aes(x = date, y = Mean, group = co2))
+
+p2 <- p + geom_line(aes(linetype = co2)) + 
+  geom_errorbar(aes(ymin = Mean - SE, ymax = Mean + SE), 
+                width = 15, size = .3,
+                position = position_dodge(20)) + 
+  geom_point(aes(shape = co2, fill = co2), position = position_dodge(20)) +
+  labs(x = "Month", y = "Nitrate") +
+  geom_vline(xintercept = as.numeric(as.Date("2012-09-18")), 
+             linetype = "dashed", col = "black") +
+  scale_x_date(breaks= date_breaks("2 month"),
+               labels = date_format("%b-%y"),
+               limits = as.Date(c("2012-6-15", "2014-4-2"))) +
+  scale_shape_manual(values = c(24, 21), labels = c("Ambient", expression(eCO[2]))) +
+  scale_fill_manual(values = c("black", "white"), 
+                    labels = c("Ambient", expression(eCO[2]))) +
+  scale_linetype_manual(values = c("solid", "dashed"), 
+                        labels = c("Ambient", expression(eCO[2]))) +
+  facet_wrap(~type, scales = "free_y", ncol = 2) +
+  science_theme
+
+# modify labels
+ylabs <- c(expression(textstyle(atop("KCl extractable", paste((mg~kg^"-1"))))),
+           expression(textstyle(atop("Soil solution (Shallow)", (mg~l^"-1")))),
+           expression(textstyle(atop("Net nitrification rate", (mg~kg^"-1"~d^"-1")))),
+           expression(textstyle(atop("IEM adsorbed", (ng~cm^"-1"~d^"-1")))),
+           expression(textstyle(atop("Soil solution (Deep)", (mg~l^"-1")))),
+           expression(textstyle(atop("Net N mineralisation rate", (mg~kg^"-1"~d^"-1")))))
+
+pl <- facet_wrap_labeller(p2, labels = ylabs)
+ggsavePP(filename = "Output//Fig/test", plot = pl, width = 7, height = 7)
+
+  
+  
+  geom_text(aes(x = xv, y = yv * .95, label = labels),
+            fontface = "bold",
+            hjust = 1,
+            data = subLabDF) +
+  facet_grid(variable~., scales= "free_y", labeller= facetLab) +
+  figTheme
--- R/Function.R
+++ R/Function.R
@@ -2,3 +2,43 @@
 # Subset and droplevles #
 #########################
 subsetD <- function(...) droplevels(subset(...))
+
+#################################
+# labells for facet_wrap graphs #
+#################################
+facet_wrap_labeller <- function(gg.plot,labels=NULL) {
+  #works with R 3.0.1 and ggplot2 0.9.3.1
+  # copied from http://stackoverflow.com/questions/19282897/
+  # how-to-add-expressions-to-labels-in-facet-wrap
+  # require(gridExtra)
+  
+  g <- ggplotGrob(gg.plot)
+  gg <- g$grobs      
+  strips <- grep("strip_t", names(gg))
+  
+  for(ii in seq_along(labels))  {
+    modgrob <- getGrob(gg[[strips[ii]]], "strip.text", 
+                       grep=TRUE, global=TRUE)
+    gg[[strips[ii]]]$children[[modgrob$name]] <- editGrob(modgrob,label=labels[ii])
+  }
+  
+  g$grobs <- gg
+  class(g) = c("arrange", "ggplot",class(g)) 
+  g
+}
+
+##############################
+# Save ggplot in PDF and PNG #
+##############################
+ggsavePP <- function(filename, plot, width, height){
+  ggsave(filename = paste(filename, ".pdf", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height)
+  
+  ggsave(filename = paste(filename, ".png", sep = ""), 
+         plot = plot, 
+         width = width, 
+         height = height, 
+         dpi = 600)
+}
